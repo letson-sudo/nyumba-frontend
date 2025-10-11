595e9193c3d02cbdd5195e420139c715402d77c0
Leckson Mpelekeni <mpelekeniinnocent@gmail.com>
2025-10-11 04:08:32 -0700

    Fix missing component imports for production build

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,6 @@ module.exports = {
         'react/require-default-props': 0,
         'react/react-in-jsx-scope': 0,
         'linebreak-style': ['error', 'unix'],
-        semi: ['error', 'never'],
+        semi: 'off',
     },
 }
